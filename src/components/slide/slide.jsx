0436cf45522655eb040383bc62671d1cddfd69c5
SpearJin <ckdwls2525@gmail.com>
2022-01-26 17:57:00 +0900

    slide 에러 수정

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -6,11 +6,12 @@ const Slide = ({ slideImages }) => {
   const [currentIndex, setCurrentIndex] = useState(2);
   const [slideWidth, setSlideWidth] = useState(0);
 
-  const [isMove, setIsMove] = useState(false);
+  const isMove = useRef(false);
   const [isLoad, setIsLoad] = useState(false);
 
   let slideList = useRef(null);
   let intervalTimeOut = useRef(null);
+  let timeOut = useRef(null);
   let reSizeTime = useRef(null);
   let touchStartX = useRef(null);
   let touchEndX = useRef(null);
@@ -38,7 +39,6 @@ const Slide = ({ slideImages }) => {
           info.style.display = 'none';
         }
       });
-      clearInterval(intervalTimeOut.current);
       intervalTime();
     });
   }, [currentIndex]);
@@ -55,6 +55,7 @@ const Slide = ({ slideImages }) => {
   // swiper 기능을 위해 마우스를 처음 클랙 햇을때 마우스를 클릭한 x좌표를 구함
   const handlerTouchStart = (e) => {
     e.preventDefault();
+
     touchStartX = e.clientX;
     document.onmousemove = drag;
   };
@@ -109,11 +110,11 @@ const Slide = ({ slideImages }) => {
   // 버튼을 연속으로 누르는걸 방지 하기 위해 isMove state값이 true일때는 return 시킴
   // 버튼을 클릭시, 다음 버튼인지 이전 버튼인지 파악하고, 조건에 맞게 nowIndex값을 바꿈
   const generateNextIndex = (e) => {
-    if (isMove) {
+    if (isMove.current) {
       return;
     }
+    isMove.current = true;
     let nextIndex;
-    setIsMove(true);
     const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.closest('.btn');
     if (target.className.includes('btn_pre')) {
       nextIndex = currentIndex - 1;
@@ -127,14 +128,17 @@ const Slide = ({ slideImages }) => {
   // 0.5초 후에 이미지가 처음이나 마지막일 경우 조건에 맞게 nowIndex값을 바꾸고, transition 값을 0으로 주고, currentIndex state값을 바꿈
   // isMove state 값도 false로 바꾸어줌
   const moveImage = (nextIndex) => {
-    slideList.current.style.transition = '300ms';
+    slideList.current.style.transition = '500ms';
+    slideList.current.style.pointerEvents = 'none';
     setCurrentIndex(nextIndex);
-    setTimeout(() => {
+    clearTimeout(timeOut.current);
+    timeOut.current = setTimeout(() => {
       nextIndex = nextIndex <= 1 ? slideImages.length - 3 : nextIndex > slideImages.length - 3 ? 2 : nextIndex;
       slideList.current.style.transition = '0s';
+      slideList.current.style.pointerEvents = 'all';
       setCurrentIndex(nextIndex);
-      setIsMove(false);
-    }, 300);
+      isMove.current = false;
+    }, 500);
   };
 
   // 인터벌이 일어나는 함수
